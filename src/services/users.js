a0feac128b0711c6da83f81b73645139a16f70a6
Dewbud <jalfonso1134@gmail.com>
2020-01-23 13:24:03 -0500

    User types and tests

--- src/services/users.js
+++ src/services/users.js
@@ -1,47 +1,58 @@
 const User = require('../models/user');
 
-// @todo: document Promise return types
+/**
+ * @typedef {Object} User
+ * @property {string} username
+ * @property {string} name
+ * @property {string} nickname
+ * @property {string} email
+ * @property {string} password hashed
+ * @property {Address[]} shipping_addresses
+ * @property {PaymentMethod[]} payment_methods
+ * @property {Date} created_at
+ * @property {Date} updated_at
+ */
 
 /**
  * @param {Object} data @todo document user model
- * @return {Promise}
+ * @return {Promise<User>}
  */
-const create = (data) => {
+const create = async (data) => {
     const new_user = new User(data);
-    return new_user.save();
+    return await new_user.save();
 };
 
 /**
  * @param {string} _id
- * @return {Promise}
+ * @return {Promise<User>}
  */
-const findById = (_id) => {
-    return User.find({ _id });
+const findById = async (_id) => {
+    return await User.findOne({ _id });
 };
 
 /**
  * @param {string} _id
- * @return {Promise}
+ * @return {Promise<User>}
  */
-const findByEmail = (email) => {
-    return User.find({ email });
+const findByEmail = async (email) => {
+    return await User.findOne({ email });
 };
 
 /**
  * @param {string} _id
- * @param {Object} data @todo document user model
- * @return {Promise}
+ * @param {User} data
+ * @return {Promise<{n: number, nModified: number}>}
  */
-const update = (_id, data) => {
-    return User.updateOne({ _id }, data);
+const update = async (_id, data) => {
+    return await User.updateOne({ _id }, data);
 };
 
 /**
  * @param {string} _id
- * @return {Promise}
+ * @return {Promise<{ok: boolean, deletedCount: number, n: number}>}
  */
-const _delete = (_id) => {
-    return User.deleteOne({ _id });
+const _delete = async (_id) => {
+    return await User.deleteOne({ _id });
 };
 
 module.exports = {
---
+++ test/users_service.js
@@ -0,0 +1,70 @@
+const assert = require('assert');
+const faker = require('faker');
+
+const Users = require('../src/services/users');
+
+describe('UsersService', () => {
+    /**
+     * @type {import('../src/services/users').User}
+     */
+    let test_user;
+    
+    describe('create()', () => {
+        it('Should create users', (done) => {
+            Users.create({
+                username: faker.hacker.noun(),
+                name: faker.name.findName(),
+                nickname: faker.name.firstName(),
+                email: faker.internet.email(),
+                password: 'S4uc3B4ws!',
+            }).then((user) => {
+                assert.notEqual(user._id, null);
+                test_user = user;
+                done();
+            });
+        });
+    });
+
+    describe('findById()', () => {
+        it('Should find users by ID', (done) => {
+            Users.findById(test_user._id)
+                .then((user) => {
+                    assert.notEqual(user._id, null);
+                    done();
+                });
+        });
+    });
+
+    describe('findByEmail()', () => {
+        it('Should find users by email', (done) => {
+            Users.findByEmail(test_user.email)
+                .then((user) => {
+                    assert.notEqual(user._id, null);
+                    done();
+                });
+        });
+    });
+
+    describe('update()', () => {
+        it('Should update users', (done) => {
+            nickname = faker.hacker.verb();
+            Users.update(test_user._id, { nickname })
+            .then((result) => {
+                assert.equal(result.n, 1);
+                assert.equal(result.nModified, 1);
+                done();
+            });
+        });
+    });
+
+    describe('deleteOne()', () => {
+        it('Should delete users', (done) => {
+            Users.delete(test_user._id)
+            .then((result) => {
+                assert.equal(result.ok, 1);
+                assert.equal(result.deletedCount, 1);
+                done();
+            });
+        });
+    });
+});
