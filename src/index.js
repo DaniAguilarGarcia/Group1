f9fbc225c84c1799f0d4a494b03a1e2c460fe469
Dewbud <jalfonso1134@gmail.com>
2020-01-23 13:23:48 -0500

    Update index.js
    Export app for API testing

--- src/index.js
+++ src/index.js
@@ -9,4 +9,6 @@ app.get('/', (req, res) => res.send('Hello World!'))
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+module.export = app;
+
 
