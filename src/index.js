cfe247c0673500a1dee374f7c26f7f2cd8893946
deellah <abdullah.diane@gmail.com>
2020-02-14 22:14:39 -0500

    created books model,books services, made changes to index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ require('dotenv').config(); // load .env
 const express = require('express');
 const app = express();
 const DB = require('./database');
+const cors = require('cors');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const bookService = require('./services/books');
+
+app.use(cors());
+app.use('/', bookService);
+
+//app.get('/', (req, res) => res.send('Hello World!'))
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
