473787770334441e1737f669f601cb8b76f198a3
Dewbud <jalfonso1134@gmail.com>
2020-02-26 11:51:06 -0500

    Add support for alternate database protocols

--- src/database.js
+++ src/database.js
@@ -5,12 +5,12 @@ const address = process.env.DB_ADDRESS;
 const port = process.env.DB_PORT;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
+const protocol = process.env.DB_PROTOCOL;
 
+let server = `${protocol}://${address}:${port}/${name}`;
 
-// let server = `mongodb://${address}:${port}/${name}`;
-let server = `mongodb+srv://Group1User:CEN4010@cen4010-group1-cluster-dcfnb.mongodb.net/test?retryWrites=true&w=majority`;
 if (user && password) {
-    server = `mongodb://${user}:${password}@${address}:${port}/${name}`;
+    server = `${protocol}://${user}:${password}@${address}:${port}/${name}`;
 }
 
 mongoose.connect(server, {
