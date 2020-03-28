92f8bb58fdcbecb021c1be45622413d2ed92aff2
Brayan Alfonso <balfo017@fiu.edu>
2020-03-28 14:31:26 -0400

    Passes book code from my independet app into the gruop project

--- src/client/src/components/MainNav.js
+++ src/client/src/components/MainNav.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './MainNav.scss';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { Nav, NavItem, NavLink } from 'react-bootstrap';
+import Search from './Search';
+import { BookConsumer } from '../pages/booksapi';
 
 class MainNav extends Component {
     constructor(props) {
         super(props);
-        this.state = Object.assign({}, props);
+
+        this.state = {
+        
+        };
     }
+
 
     static getDerivedStateFromProps(props, state) {
       return props;
@@ -46,23 +54,57 @@ class MainNav extends Component {
     }
 
     render() {
+
         return (
+            <container>
             <nav className="navbar navbar-expand-lg navbar-light bg-light row">
                 <Link to={'/'} className="navbar-brand">
                     Book Geeks
                 </Link>
+
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <form className="form-inline my-2 flex-grow-1">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
+                    <input type ="text" 
+                        value={this.state.search}
+                        onChange={(e) => this.searchCallBack(e.target.value)}/>
+                    
+                       
+  <Nav className="justify-content-center" activeKey="/home">
+    <Nav.Item>
+      <Dropdown as={NavItem}>
+        <Dropdown.Toggle as={NavLink}>Genres</Dropdown.Toggle>
+        <Dropdown.Menu>
+        <Dropdown.Item>Fiction</Dropdown.Item>
+        <Dropdown.Item>Non-Fiction</Dropdown.Item>
+        </Dropdown.Menu>
+        </Dropdown>
+    </Nav.Item>
+    <Nav.Item>
+    <Dropdown as={NavItem}>
+        <Dropdown.Toggle as={NavLink}>Ratings</Dropdown.Toggle>
+        <Dropdown.Menu>
+        <Dropdown.Item>Five Star</Dropdown.Item>
+        <Dropdown.Item>Four Star</Dropdown.Item>
+        <Dropdown.Item>Three Star</Dropdown.Item>
+        <Dropdown.Item>Two Star</Dropdown.Item>
+        <Dropdown.Item><i class="fa fa-star" size="2x" color='yellow'/></Dropdown.Item>
+        </Dropdown.Menu>
+        </Dropdown>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-3">Top Sellers</Nav.Link>
+    </Nav.Item>
+  </Nav>
+        </form>
+                    
                     {this.navRight()}
                 </div>
             </nav>
+            </container>
         );
     }
 }
--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
+import {BookProvider} from './pages/booksapi';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render((
+    <BookProvider>
     <BrowserRouter>
         <App/>
     </BrowserRouter>
-), document.getElementById('root'));
+    </BookProvider>
+), 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
